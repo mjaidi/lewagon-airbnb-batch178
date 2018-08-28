e0b5771b54a66828075dea91d0c8fd1f4c107ea8
Yassmina Hannaoui <yas_h27@hotmail.fr>
2018-08-28 16:25:12 +0100

    done with seeding the cards and avatars

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
  skip_before_action :authenticate_user!, only: :home
   def home
     @search = false
+    @apartments = policy_scope(Apartment)
+    @apartments = @apartments.sample(6)
   end
 end
