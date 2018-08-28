be7c0d80cba9c8b926a7364bcee6652715245856
Majid Jaidi <majid.jaidi@gmail.com>
2018-08-28 10:49:02 +0100

    setup search controller and skeletton of search page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'pages/search', to: "pages#search", as: :search
   resources :apartments do
     resources :bookings, except: [:edit, :update] do
       resources :reviews
