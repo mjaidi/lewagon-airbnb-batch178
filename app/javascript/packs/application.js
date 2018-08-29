8f92e93fc5119a1d6c5b85119b21457e771a4e4c
Majid Jaidi <majid.jaidi@gmail.com>
2018-08-29 14:42:47 +0100

    added typed on home

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,8 @@
-
 import "bootstrap";
 import { initUpdateNavbarOnScroll } from "../pages/navbar";
+import { loadDynamicBannerText } from '../pages/banner';
 
+
+
+loadDynamicBannerText();
 initUpdateNavbarOnScroll();
