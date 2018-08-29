bd1fbdbb9094379b9f8b07c633615b816d7d9488
Majid Jaidi <majid.jaidi@gmail.com>
2018-08-29 08:37:06 +0100

    working map with markers on local server

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -9,4 +9,7 @@ class Apartment < ApplicationRecord
   validates :description, presence: true
   validates :address, presence: true
   validates :price_per_day, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_104019) do
+ActiveRecord::Schema.define(version: 2018_08_29_064151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2018_08_28_104019) do
     t.integer "service_fees"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_apartments_on_user_id"
   end
 
