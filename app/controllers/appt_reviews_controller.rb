4ebeac1124869038664c1d179766a314ab228dce
Majid Jaidi <majid.jaidi@gmail.com>
2018-08-31 15:24:13 +0100

    minor error fix

--- app/controllers/appt_reviews_controller.rb
+++ app/controllers/appt_reviews_controller.rb
@@ -3,6 +3,7 @@ class ApptReviewsController < ApplicationController
   def create
     @appt_review = ApptReview.new(appt_review_params)
     @appt_review.booking_id = params[:booking_id]
+    
     if @appt_review.save
       redirect_to apartment_path(Apartment.find(params[:apartment_id]))
     else
